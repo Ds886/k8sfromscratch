99f7dc2148fb4577d093f0f824cf76713b6f783d
Ds886 <2217864+Ds886@users.noreply.github.com>
2023-12-13 00:48:23 +0200

    * Set loading order and logging

--- kfs-operator.sh
+++ kfs-operator.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-
-. ./vars.sh
+set -eux
+DATE="$(date)"
+printf "[%s] %s - %s\n"  "-" "${DATE}"  "Staring operator cli"
 . ./utils.sh
+sys_log_trace "Checking dependencies"
+. ./deps.sh
+sys_log_trace "loading vars"
+. ./vars.sh
+sys_log_trace "loading certs"
 . ./certs.sh
 
+sys_log_trace "success import"
+
+sc_cert_gen_all
 
