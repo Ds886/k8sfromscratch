bfeefd70bf08b6fd1e3cb70a82aee754a0495fbf
Ds886 <2217864+Ds886@users.noreply.github.com>
2023-12-13 23:31:34 +0200

    * increase deps and split them between controlplane and rmeotehosts

--- kfs-deps.sh
+++ kfs-deps.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-DEPS_LIST="cfssl cfssljson"
+_DEPS_DEPS_RUNNER=ssh sshpass scp base64
+_DEPS_DEPS_LIST="cfssl cfssljson python3 base64"
+_DEPS_DEPS_OPTIONAL="curl"
 
-for DEP in $DEPS_LIST; do
+deps_prepare_host()
+{
+  sys_log_trace "Changing repositories to edge"
+  echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" | tee /etc/apk/repositories
+  echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" | tee -a /etc/apk/repositories
+  echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" | tee -a /etc/apk/repositories
+
+  sys_log_trace "Installing ${PACKAGES_INFRA}"
+  # shellcheck disable=2086
+  apk update && apk upgrade && apk add ${PACKAGES_INFRA}
+
+  sc_change_hostname "${K8S_HOSTS_CONTR_NAME}"
+}
+
+for DEP in $_DEPS_DEPS_LIST; do
   set +ue
   COMM=$(command -v "$DEP")
   if [ -z "${COMM}" ]; then
